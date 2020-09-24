52f254b68d55356912e87e319742be5b739f819d
Alexis <lagodka.alexis@gmail.com>
2020-09-24 21:28:33 +0200

    toGithub

--- dotpointer.js
+++ dotpointer.js
@@ -1,14 +1,21 @@
-window.addEventListener('mousemove', function(e){
-    document.getElementById("dot").style.display = "block";
-    document.getElementById("dot").style.left = e.pageX + "px";
-    document.getElementById("dot").style.top = e.pageY + "px";
-});
-
-window.addEventListener('mouseout', function(e){
-    document.getElementById("dot").style.display = "none";
-});
-
 $(document).ready(function(){
+    
+    $(document).bind("mousemove", function(e) {
+        //$("#dot").css("background-color", "greenyellow");
+        document.getElementById("dot").style.left = e.pageX + "px";
+        document.getElementById("dot").style.top = e.pageY + "px";
+    });
+
+    $(document).mouseenter(function() {
+        document.getElementById("dot").style.display = "block";
+        $("#dot").css("background-color", "greenyellow");
+    });
+    $(document).mouseleave(function() {
+        document.getElementById("dot").style.display = "none";
+    });
+
+
+
     $("a").hover(function(){
         $("#dot").css("background-color", "red");
             }, function(){
